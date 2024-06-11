edb4ffc8c57b31439c8f9a139c8fdf3dd4d33095
Adam Zieliński <adam@adamziel.com>
2024-06-11 10:51:03 +0200

    Batch create pages

--- playground-markdown/playground-markdown.js
+++ playground-markdown/playground-markdown.js
@@ -15,39 +15,31 @@
 	const { markdownToBlocks } = await import(
 		'../blocky-formats/src/markdown.js'
 	);
-
-	const parents = {};
-
-	for (let file of window.playgroundMarkdown.markdown) {
-		const content = file.content
-			? markdownToBlocks(file.content).map((block) =>
-					wp.blocks.serializeRawBlock({
-						blockName: block.name,
-						attrs: block.attributes,
-						innerBlocks: block.innerBlocks,
-						innerContent: [block.attributes.content],
-					})
-			  )
-			: [];
-		const postData = {
-			title: file.name,
-			content: content.join(''),
-			status: 'publish',
-        };
-		const parentPath = file.path.split('/').slice(0, -1).join('/');
-        if (parentPath in parents) {
-            parents[parentPath].parent = parents[parentPath].id;
-        }
-		const page = await fetch('/wp-json/wp/v2/pages', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'X-WP-Nonce': wpApiSettings.nonce,
-			},
-			body: JSON.stringify(postData),
-		});
-		parents[file.path] = (await page.json()).id;
-	}
+    
+    await fetch('/wp-json/wp/v2/page-hierarchy', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-WP-Nonce': wpApiSettings.nonce,
+        },
+        body: JSON.stringify({
+            pages: window.playgroundMarkdown.markdown.map((file) => ({
+                ...file,
+                content: (
+                    file.content
+                        ? markdownToBlocks(file.content).map((block) =>
+                            wp.blocks.serializeRawBlock({
+                                blockName: block.name,
+                                attrs: block.attributes,
+                                innerBlocks: block.innerBlocks,
+                                innerContent: [block.attributes.content],
+                            })
+                        )
+                        : []
+                ).join("\n"),
+            }))
+        }),
+    })
 
 	if (window.location.pathname !== '/category/uncategorized/') {
 		window.open('/category/uncategorized/', '_self');
